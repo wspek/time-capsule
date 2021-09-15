67f577b21c3be4f28c53aed890b31790b2ef1a1e
Waldo @ Pi <waldo@raspberry.pi>
2021-09-14 20:33:00 -0400

    Remote tweaks

--- time_capsule.py
+++ time_capsule.py
@@ -5,7 +5,11 @@ import logging
 from config import config
 from resources import Resource
 
-logging.basicConfig(filename=config['logging']['outfile'], level=config['logging']['level'])
+logging.basicConfig(
+    filename=config['logging']['outfile'], 
+    level=config['logging']['level'], 
+    format='%(asctime)s %(levelname)-8s %(message)s', 
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def main():
