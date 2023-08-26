6f443d8120cea2865fa2000577d0ca5603002aac
Waldo Spek <waldospek@gmail.com>
2023-08-26 13:46:18 -0300

    Adapt code to Google App Password

--- emails.py
+++ emails.py
@@ -6,9 +6,5 @@ from config import config
 def send_email(to, subject, body):
     sender_config = config['email']['sender']
 
-    yag = yagmail.SMTP({sender_config['address']: sender_config['name']}, sender_config['password'])
-    yag.send(
-        to=to,
-        subject=subject,
-        contents=body,
-    )
+    yag = yagmail.SMTP(user=sender_config['address'], password=sender_config['password'])
+    yag.send(to=to, subject=subject, contents=body)
